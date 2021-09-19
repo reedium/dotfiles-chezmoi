10d4da9102ac7b15c46696afff0e0ab151579995
Ryan Reed <git@ryanreed.net>
2021-09-19 13:32:34 -0400

    Formatting

--- dot_zsh/configs/keybindings.zsh
+++ dot_zsh/configs/keybindings.zsh
@@ -15,20 +15,20 @@ zle -N down-line-or-beginning-search
 
 case "${TERM}" in
     (cons25*|linux)
-		bindkey '^[[1~'   beginning-of-line   # HOME
-		bindkey '^[[4~'   end-of-line         # END
+        bindkey '^[[1~'   beginning-of-line   # HOME
+        bindkey '^[[4~'   end-of-line         # END
         bindkey '\e[5~'   delete-char         # DELETE
         bindkey '\e[A' up-line-or-beginning-search      # UP
         bindkey '\e[B' down-line-or-beginning-search    # DOWN
     ;;
     (*xterm*)
-		bindkey '\e[H'    beginning-of-line   # HOME
-		bindkey '\e[F'    end-of-line         # END
-		bindkey '\e[1~'   beginning-of-line   # HOME (Workaround for screen under tmux)
-		bindkey '\e[4~'   end-of-line         # END (Workaround for screen under tmux)
-		bindkey '\e[3~'   delete-char         # DELETE
-		bindkey '\e[1;5C' forward-word        # CTRL+RIGHT
-		bindkey '\e[1;5D' backward-word       # CTRL+LEFT
+        bindkey '\e[H'    beginning-of-line   # HOME
+        bindkey '\e[F'    end-of-line         # END
+        bindkey '\e[1~'   beginning-of-line   # HOME (Workaround for screen under tmux)
+        bindkey '\e[4~'   end-of-line         # END (Workaround for screen under tmux)
+        bindkey '\e[3~'   delete-char         # DELETE
+        bindkey '\e[1;5C' forward-word        # CTRL+RIGHT
+        bindkey '\e[1;5D' backward-word       # CTRL+LEFT
         bindkey '^[[A' history-beginning-search-backward    # UP
         bindkey '^[[B' history-beginning-search-forward     # DOWN
     ;;
