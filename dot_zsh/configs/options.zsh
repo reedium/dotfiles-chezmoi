b595e5918ec257ce13121277ee6b55699ad8ef34
Ryan Reed <git@ryanreed.net>
2021-09-19 13:52:46 -0400

    Updating ZSH Options and autocompletion

--- dot_zsh/configs/options.zsh
+++ dot_zsh/configs/options.zsh
@@ -1,21 +1,31 @@
 setopt NO_CASE_GLOB
 
-# Allow for suggesting corrections on mistyped commands/files
-setopt CORRECT
-setopt CORRECT_ALL
-
-setopt auto_list     # Automatically list choices on ambiguous completion
-setopt always_to_end # Move cursor to end if word had only 1 match
-setopt auto_menu     # Automatically use menu completion
 
 # AUTOCOMPLETION RELATED
 # zstyles and settings should come BEFORE the compinit autoload
-setopt autocd  # cd if directory (e.g. forgot 'cd' in command)
+zstyle ':completion:*' completer _expand _complete _correct _approximate _history
+zstyle ':completion:*' matcher-list '' '' 'l:|=* r:|=*' 'l:|=* r:|=*'
+zstyle ':completion:*' list-colors ''
+zstyle ':completion:*' users root
+zstyle ':completion:*' menu select
+zstyle :compinstall filename '~/.zshrc'
+autoload -Uz compinit
+autoload -U colors && colors
+compinit
 
-zstyle ':completion:*' matcher-list 'm:{[:lower:][:upper:]}={[:upper:][:lower:]}' 'm:{[:lower:][:upper:]}={[:upper:][:lower:]} l:|=* r:|=*' 'm:{[:lower:][:upper:]}={[:upper:][:lower:]} l:|=* r:|=*' 'm:{[:lower:][:upper:]}={[:upper:][:lower:]} l:|=* r:|=*' # Case insensitive completion
-zstyle ':completion:*' list-suffixes zstyle ':completion:*' expand prefix suffix  # Partial/fragmented completion
-zstyle ':completion:*' menu select # select completions with arrow keys
-zstyle ':completion:*' group-name '' # group results by category
-zstyle ':completion:::::' completer _expand _complete _ignored _approximate # enable approximate matches for completion
 
-autoload -Uz compinit && compinit
+# Other Options
+unsetopt beep
+setopt no_beep
+setopt autocd       # cd if directory (e.g. forgot 'cd' in command)
+
+setopt auto_menu        # Automatically use menu completion
+setopt auto_list        # Automatically list choices on ambiguous completion
+setopt list_rows_first  # List autocompletions horizontally rather than vertically
+setopt always_to_end    # Move cursor to end if word had only 1 match
+
+setopt no_hup           # Allow backgrounding
+
+# Allow for suggesting corrections on mistyped commands/files
+setopt CORRECT
+setopt CORRECT_ALL
